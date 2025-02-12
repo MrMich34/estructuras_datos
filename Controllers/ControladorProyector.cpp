09b9bca870c0f1b6c1f4b31c8f2edd0ec75bcd97
JohnWilliams2050 <109265994+JohnWilliams2050@users.noreply.github.com>
2025-02-11 18:16:14 -0800

    Update ControladorProyector.cpp

--- Controllers/ControladorProyector.cpp
+++ Controllers/ControladorProyector.cpp
@@ -11,14 +11,23 @@ private:
     Imagen imagenActual;
 
 public:
-    // Metodo para codificar una imagen y guardarla en un archivo
+    /**
+    *Codifica una imagen y guardarla en un archivo
+    *@param string nombreSalida
+    *@return none
+    */ 
     void codificarImagen(std::string& nombreSalida) {
         std::cout << "Llamada a codificarImagen con nombreSalida: " << nombreSalida << std::endl;
         // Se simula la codificacion de la imagen
         codificador.codificarImagen(nombreSalida);
     }
 
-    // Método para decodificar una imagen desde un archivo y guardarla
+    /**
+    *Decodifica una imagen desde un archivo y guardarla
+    *@param string nombreArchivo
+    *@param string nombreSalida
+    *@return none
+    */
     void decodificarArchivo(std::string& nombreArchivo, std::string& nombreSalida) {
         std::cout << "Llamada a decodificarArchivo con nombreArchivo: " << nombreArchivo << " y nombreSalida: " << nombreSalida << std::endl;
         // Se simula la decodificacion de la imagen
