d4974d14fa553b9859b6cacf663348dafbc35c43
GabrielJaramilloCuberos <124003921+GabrielJaramilloCuberos@users.noreply.github.com>
2025-02-11 22:15:43 -0500

    Update ConsolaVista.h

--- Views/ConsolaVista.h
+++ Views/ConsolaVista.h
@@ -8,32 +8,9 @@
 
 class ConsolaVista {
 public:
-    /**
-     * Muestra un mensaje en la consola
-     * @param std::string& mensaje - Mensaje a mostrar
-     * @return void
-     */
     void mostrarMensaje(const std::string& mensaje);
-    
-    /**
-     * Muestra un mensaje de error en la consola
-     * @param std::string& mensaje - Mensaje de error a mostrar
-     * @return void
-     */
     void mostrarError(const std::string& mensaje);
-    
-    /**
-     * Muestra la información de una imagen en la consola
-     * @param Imagen& imagen - Imagen de la cual se mostrará la información
-     * @return void
-     */
     void mostrarInfoImagen(Imagen& imagen);
-    
-    /**
-     * Muestra la información de un volumen de imágenes en la consola
-     * @param VolumenImagenes& volumen - Volumen de imágenes del cual se mostrará la información
-     * @return void
-     */
     void mostrarInfoVolumen(VolumenImagenes& volumen);
 };
 
